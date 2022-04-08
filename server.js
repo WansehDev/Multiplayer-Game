d4d7d94d8b896aa1fb08f368b1a46619ccdadaaf
LanceParantar <20180014283@my.xu.edu.ph>
2022-04-08 17:30:07 +0800

    v2
    fix multiplayer game

--- server.js
+++ server.js
@@ -12,22 +12,44 @@ app.set("view engine", "ejs");
 
 const server = app.listen(1337);
 const io = require("socket.io")(server);
-const users = [];
-const players = [];
-app.get("/", function(req, res) {
-    res.render("index");
-});
-
-
+let score = 0;
+let allUsers = [];
+let pos = null;
 
 io.on("connection", (socket) => {
-    console.log("User Connected Count:" , io.engine.clientsCount);
-    console.log(socket.id);
-    //disconnect users
-    
-    
-    socket.on("disconnect", () => {
-        console.log("User Disconnected Count:" , io.engine.clientsCount);
-    })
+  console.log("User Connected Count:", io.engine.clientsCount);
 
+  socket.on("newPlayer", (data) => {
+    allUsers.push(data.newPlayer);
+    io.emit("displayActiveUsers", { users: allUsers });
+  });
+
+  io.emit("displayActiveUsers", { users: allUsers });
+
+  socket.on("updateScore", (data) => {
+    console.log("updateScore", data);
+    score = data.score;
+    for (let users of allUsers) {
+        if (users.id === data.id) {
+            users.score = data.score;
+        }
+        // score if 10000
+        if (users.score === 10000) {
+            io.emit("winner" , { winnerUser : users});
+            break;
+        }
+    }
+    io.emit("displayScore", { users: allUsers });
+  });
+  io.emit("displayScore", { users: allUsers });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected Count:", io.engine.clientsCount);
+    allUsers.pop();
+    io.emit("displayActiveUsers", { users: allUsers });
+  });
+  
+});
+app.get("/", function (req, res) {
+  res.render("index");
 });
--- static/js/player.js
+++ static/js/player.js
@@ -1,3 +1,5 @@
+var player = null; //global variable player
+
 document.addEventListener("DOMContentLoaded", function () {
   /*
    * Class Player
@@ -18,30 +20,64 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  class Enemy {
+    constructor(x, y, speed, type) {
+      this.x = x;
+      this.y = y;
+      this.speed = speed;
+      this.type = type;
+    }
+  }
+
+  let name = prompt("Enter your name");
+
   const socket = io();
   const div = document.getElementById("container");
-  let player = null;
+  const onlineList = document.getElementById("online-list");
   let elementPlayer = null;
   let pos = { x: 0, y: 0 };
 
-  socket.on("connect", () => {
-    player = new Player(socket.id, "Player", 100, 0);
+  socket.on("connect", function () {
+    player = new Player(socket.id, name, 100, 0);
 
     // create element for player
     let playerDiv = document.createElement("div");
     playerDiv.id = player.id;
     playerDiv.className = "sprite-hero";
-    playerDiv.innerHTML = `<span >${player.name}</span>`;
+    playerDiv.innerHTML = `<span class=nametag>${player.name}</span>`;
     playerDiv.style.left = player.x + "px";
     playerDiv.style.top = player.y + "px";
 
     // add the playerDiv to container
     div.appendChild(playerDiv);
-    elementPlayer = document.getElementById(player.id);
+
+    // send player to server
+    socket.emit("newPlayer", { newPlayer: player });
 
     // set the player's position to variable pos
     pos.x = player.x;
     pos.y = player.y;
+
+    // get the player's element and set the it to elementPlayer
+    elementPlayer = document.getElementById(player.id);
+  });
+
+  // check winner
+  socket.on("winner", (data) => {
+    alert(`${data.winnerUser.name} won the game!`);
+    location.reload();
+  });
+
+  // display active users
+  socket.on("displayActiveUsers", (data) => {
+    onlineList.innerHTML = "";
+    let users = data.users;
+    for (let user of users) {
+      let onlineListItem = document.createElement("li");
+      onlineListItem.id = "_" + user.id;
+      onlineListItem.innerHTML = `${user.name} - ${user.score}`;
+      onlineList.appendChild(onlineListItem);
+    }
   });
 
   function eventMovePlayer(event) {
@@ -74,7 +110,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // fire event when player presses spacebar
   function eventFireBullet() {
-    player.bullets.push({ x: player.x + 46 , y: player.y + 10 });
+    player.bullets.push({ x: player.x + 46, y: player.y + 10 });
   }
   // draw bullets
   function drawBullets() {
@@ -98,12 +134,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  function loop() {
-    moveBullets();
-  }
-
-  setInterval(loop, 2);
-
   function checkBoundaries() {
     if (pos.x < 0) {
       pos.x = 0;
@@ -126,4 +156,77 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // event listener for keydown
   document.addEventListener("keydown", eventMovePlayer);
+
+  // Enemies class
+
+  const enemyTypes = ["sprite-enemy1", "sprite-enemy2", "sprite-enemy3"];
+  const speeds = [5, 6, 7, 8, 9, 10, 11, 12];
+  var enemies = [];
+
+  // create enemies
+  for (let i = 0; i < 10; i++) {
+    let x = Math.floor(Math.random() * (860 - 100) + 100);
+    let y = 30;
+    let speed = speeds[Math.floor(Math.random() * speeds.length)];
+    let type = enemyTypes[Math.floor(Math.random() * enemyTypes.length)];
+    enemies.push(new Enemy(x, y, speed, type));
+  }
+
+  // display enemies
+  function drawEnemies() {
+    let output = "";
+    for (let enemy of enemies) {
+      output += `<div class="${enemy.type}" 
+                    style="left:${enemy.x}px;
+                    top:${enemy.y}px;">
+                    </div>`;
+    }
+    document.getElementById("enemies").innerHTML = output;
+  }
+
+  function moveEnemies() {
+    for (let enemy of enemies) {
+      enemy.y += enemy.speed;
+      drawEnemies();
+      if (enemy.y > 600) {
+        enemy.y = 30;
+        enemy.x = Math.floor(Math.random() * (860 - 100) + 100);
+      }
+    }
+  }
+
+  // Collision detection
+  function checkCollision() {
+    for (let enemy of enemies) {
+      for (let bullet of player.bullets) {
+        if (
+          bullet.x > enemy.x &&
+          bullet.x < enemy.x + 20 &&
+          bullet.y > enemy.y &&
+          bullet.y < enemy.y + 20
+        ) {
+          // restart the enemy
+          enemy.y = 30;
+          enemy.x = Math.floor(Math.random() * (860 - 100) + 100);
+
+          player.score += 100;
+          document.getElementById("scoretag").innerHTML = player.score;
+
+          socket.emit("updateScore", { score: player.score, id: player.id });
+          socket.on("displayScore", (data) => {
+            for (let user of data.users) {
+              let userDiv = document.getElementById("_" + user.id);
+              userDiv.innerHTML = `${user.name} - ${user.score}`;
+            }
+          });
+        }
+      }
+    }
+  }
+
+  setInterval(() => {
+    moveBullets();
+    checkCollision();
+  }, 2);
+  setInterval(moveEnemies, 40);
 });
